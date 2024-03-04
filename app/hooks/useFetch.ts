b2205c8f283f4f062c344a5cbf582a05f9ba37e3
FayzullayevAbbos <abbosfayzullayev202@gmail.com>
2024-03-04 22:20:54 +0500

    last

--- app/hooks/useFetch.ts
+++ app/hooks/useFetch.ts
@@ -1,7 +1,7 @@
 import Product from "../types/dataType";
 
 async function getData(): Promise<Product[]> {
-  const res = await fetch('https://audiophile-8.vercel.app');
+  const res = await fetch('https://audiophile-8.vercel.app/api');
   return await res.json();
 }
 export default getData;
