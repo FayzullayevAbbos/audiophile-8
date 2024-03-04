d15a281bc30d9fb6a51faddf5fcc7cb191c0de0e
FayzullayevAbbos <abbosfayzullayev202@gmail.com>
2024-03-04 22:13:43 +0500

    api changed

--- app/hooks/useFetch.ts
+++ app/hooks/useFetch.ts
@@ -1,7 +1,7 @@
 import Product from "../types/dataType";
 
 async function getData(): Promise<Product[]> {
-  const res = await fetch(process.env.BASE_URL + "/api");
+  const res = await fetch('https://audiophile-8.vercel.app');
   return await res.json();
 }
 export default getData;
